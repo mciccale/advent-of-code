1b7e26740890e54a6b1627c9ebb140f28ca387d6
Marco Ciccalè Baztán <marcociccalebaztan@gmail.com>
2025-12-03 10:29:14 +0100

    naive solution to second day (part 1)

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,16 +1,35 @@
 use advent_of_code::rawln;
+use std::ops::Range;
 
-fn easy(lines: &[&str]) -> i32 {
-    todo!("The easy problem is still not solved!");
+fn invalid_id(n: u64) -> bool {
+    let s = n.to_string();
+    let l = s.len();
+    l % 2 == 0 && &s[..l/2] == &s[l/2..]
 }
 
-fn hard(lines: &[&str]) -> i32 {
+fn easy(ranges: &[Range<u64>]) -> u64 {
+    ranges
+        .iter()
+        .map(|r| r.clone().filter(|&n| invalid_id(n)).sum::<u64>())
+        .sum()
+}
+
+fn hard(ranges: &[Range<u64>]) -> u64 {
     todo!("The easy problem is still not solved!");
 }
 
 fn main() {
-    let raw: String = rawln().pop().unwrap(); // input is a single line
-    let ranges: Vec<&str> = raw.split(',').collect();
-    println!("Part 1: {}", easy(&ranges));
-    println!("Part 2: {}", hard(&ranges));
+    let raw: String = rawln().pop().unwrap();
+    let ranges: Vec<Range<u64>> = raw
+        .split(',')
+        .map(|r| {
+            let (start, end) = r.split_once('-').unwrap();
+            Range {
+                start: start.parse::<u64>().unwrap(),
+                end: end.parse::<u64>().unwrap() + 1,
+            }
+        })
+        .collect();
+    println!("Part 1: {:?}", easy(&ranges));
+    // println!("Part 2: {}", hard(&ranges));
 }
